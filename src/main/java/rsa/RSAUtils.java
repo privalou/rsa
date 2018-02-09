97e3bb6ca2dd5e093a5aace961331557125e7fdf
Artem <a.privalov63@gmail.com>
2018-02-09 11:45:24 +0400

    dududu

--- src/main/java/rsa/RSAUtils.java
+++ src/main/java/rsa/RSAUtils.java
@@ -4,12 +4,10 @@ import java.lang.reflect.Field;
 import java.math.BigInteger;
 
 public class RSAUtils {
-   private static Field bigIntegerData;
 
    public static byte[] modPowByte(byte[] arg, BigInteger e, BigInteger n) {
       BigInteger source = new BigInteger(1, arg);
       BigInteger result = source.modPow(e, n);
-      hideBigInteger(source);
       return getBytesWithoutSign(result);
    }
 
@@ -24,18 +22,4 @@ public class RSAUtils {
       }
    }
 
-   public static void hideBigInteger(BigInteger source) {
-      try {
-         if (bigIntegerData == null) {
-            bigIntegerData = BigInteger.class.getDeclaredField("mag");
-            bigIntegerData.setAccessible(true);
-         }
-         int[] mag = (int[])bigIntegerData.get(source);
-         for (int i = 0; i < mag.length; i++) {
-            mag[i] = 0;
-         }
-      } catch (NoSuchFieldException | IllegalAccessException e) {
-         System.err.println("[Warning] Parts of the plaintext may remain in memory");
-      }
-   }
 }
